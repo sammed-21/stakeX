889e74eeda17da04a5c41a6ef72f279fad955409
Sammed S Betadur <sammedsbetadur@Sammeds-MacBook-Pro.local>
2024-10-14 01:33:21 +0530

    add the wallet connection

---
+++ wagmi.ts
@@ -0,0 +1,23 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+
+export const config = getDefaultConfig({
+  appName: "StakeX",
+  projectId: "be70ce90517c29ac277e795db970fbf0",
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  ssr: true,
+});
